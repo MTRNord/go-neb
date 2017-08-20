17509b89b855de1656632235e1b018d4e5ef5a07
MTRNord <mtrnord1@gmail.com>
2017-08-20 16:56:34 +0200

    Add very basic !freifunk communities

---
+++ src/github.com/matrix-org/go-neb/services/freifunk/freifunk.go
@@ -0,0 +1,73 @@
+// Package freifunk implements a Service which gives access to freifunk map nodes.
+package freifunk
+
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/buger/jsonparser"
+	"github.com/matrix-org/go-neb/types"
+	"github.com/matrix-org/gomatrix"
+	"github.com/prometheus/common/log"
+)
+
+// ServiceType of the Freifunk service
+const ServiceType = "freifunk"
+
+// Service represents the Echo service. It has no Config fields.
+type Service struct {
+	types.DefaultService
+}
+
+// Commands supported:
+//    !freifunk communities
+// Responds with a notice of a list with all communities.
+func (s *Service) Commands(cli *gomatrix.Client) []types.Command {
+	return []types.Command{
+		types.Command{
+			Path: []string{"freifunk", "communities"},
+			Command: func(roomID, userID string, args []string) (interface{}, error) {
+				return getCommunities()
+			},
+		},
+	}
+}
+
+func getCommunities() (interface{}, error) {
+	var communities []byte
+	var handler func([]byte, []byte, jsonparser.ValueType, int) error
+	handler = func(key []byte, value []byte, dataType jsonparser.ValueType, offset int) error {
+		communities = append(communities, "\n", key)
+	}
+	jsonparser.ObjectEach(getFFApi(), handler)
+	return &gomatrix.TextMessage{"m.notice", jsonparser.ParseString(communities)}, nil
+}
+
+// searchGiphy returns info about a gif
+func (s *Service) getFFApi() ([]byte, error) {
+	log.Info("Fetching FF API File for ")
+	u, err := url.Parse("https://api.freifunk.net/data/ffSummarizedDir.json")
+	if err != nil {
+		return nil, err
+	}
+	q := u.Query()
+	u.RawQuery = q.Encode()
+	res, err := http.Get(u.String())
+	if res != nil {
+		defer res.Body.Close()
+	}
+	if err != nil {
+		return nil, err
+	}
+	data := []byte(res.Body)
+
+	return data, nil
+}
+
+func init() {
+	types.RegisterService(func(serviceID, serviceUserID, webhookEndpointURL string) types.Service {
+		return &Service{
+			DefaultService: types.NewDefaultService(serviceID, serviceUserID, ServiceType),
+		}
+	})
+}
